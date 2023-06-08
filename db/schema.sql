de67e0f0d2f95e9a8a63e9de4f0afa5374632764
Alycia Oliveira <aly.oliveira@spectrumeffect.com>
2023-06-08 16:47:12 -0600

    rebuilding the cli logic

--- db/schema.sql
+++ db/schema.sql
@@ -4,32 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  department_id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (department_id)
+  id INT NOT NULL PRIMARY KEY,
+  department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  role_id INT NOT NULL AUTO_INCREMENT,
-  department_id INT NOT NULL,
-  title VARCHAR(30),
-  salary DECIMAL,
-  PRIMARY KEY (role_id),
-  FOREIGN KEY (department_id)
-  REFERENCES department(department_id)
+  id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-
-
 CREATE TABLE employee (
-  employee_id INT NOT NULL AUTO_INCREMENT,
-  role_id INT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT,
-  PRIMARY KEY (employee_id),
-  FOREIGN KEY (role_id)
-  REFERENCES role(role_id),
-  FOREIGN KEY (manager_id) 
-  REFERENCES employee(employee_id)
+  id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 -- Insert departments
-INSERT IGNORE INTO department (department_id, department_name)
+INSERT IGNORE INTO department (id, department_name)
 VALUES (1, 'Engineering'),
     (2, 'Finance'),
     (3, 'Legal'),
@@ -7,7 +7,7 @@ VALUES (1, 'Engineering'),
     (5, 'Service');
 
 -- Insert roles
-INSERT IGNORE INTO role (role_id, title, salary, department_id)
+INSERT IGNORE INTO role (id, title, salary, department_id)
 VALUES (1, 'Sales Lead', 8000.00, 4),
     (2, 'Salesperson', 4000.00, 4),
     (3, 'Lead Engineer', 8000.00, 1),
@@ -19,10 +19,11 @@ VALUES (1, 'Sales Lead', 8000.00, 4),
     (9, 'Customer Service', 4000.00, 5);
 
 -- Insert employees
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, 1),
-    ('Kevin', 'Tupik', 4, 1),
-    ('Kunal', 'Singh', 5, 1),
-    ('Malia', 'Brown', 6, 1);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'John', 'Doe', 1, NULL),
+    (2, 'Mike', 'Chan', 2, 1),
+    (3, 'Ashley', 'Rodriguez', 3, 1),
+    (4, 'Kevin', 'Tupik', 4, 1),
+    (5, 'Kunal', 'Singh', 5, 1),
+    (6, 'Malia', 'Brown', 6, 1);
+
